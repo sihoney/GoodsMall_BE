699186e1fa5cca4940799e0989941edb3a4c3c98
park9707 <sodkeldla@gmail.com>
2026-04-01 11:28:21 +0900

    refactor: 사용하지 않는 import 제거

--- order/src/main/java/com/example/order/infrastructure/repository/DeliveryJpaRepository.java
+++ order/src/main/java/com/example/order/infrastructure/repository/DeliveryJpaRepository.java
@@ -3,7 +3,6 @@ package com.example.order.infrastructure.repository;
 import com.example.order.domain.entity.Delivery;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
