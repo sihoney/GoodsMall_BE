6d078e350e8f16287d941801ca405c19b498c431
yellow-pang <troh0322@naver.com>
2026-04-02 16:49:44 +0900

    refactor: kafka 사용 및 member-payment 이벤트 계약 수정
    - @EnableKafka 빠진 추가하여 이벤트 발행 및 구독 설정 완료
    - payment에서 member 발행한 이벤트 구독하도록 email 빠진 부분 추가

--- payment/src/main/java/com/example/payment/PaymentApplication.java
+++ payment/src/main/java/com/example/payment/PaymentApplication.java
@@ -3,10 +3,12 @@ package com.example.payment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableKafka
 @EnableScheduling
 public class PaymentApplication {
     public static void main(String[] args) {
