9d5f3fb7e3ae93c562a5d02c180ea33f069cc453
황희원 <82200359+sihoney@users.noreply.github.com>
2026-04-02 09:16:04 +0900

    feat: member 모듈의 MemberSignedUp 생성시간 타입을 Instant로 변경

--- member/src/main/java/com/example/member/application/event/MemberSignedUpEvent.java
+++ member/src/main/java/com/example/member/application/event/MemberSignedUpEvent.java
@@ -1,12 +1,12 @@
 package com.example.member.application.event;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 import java.util.UUID;
 
 public record MemberSignedUpEvent(
         UUID eventId,
         UUID memberId,
         String email,
-        LocalDateTime occurredAt
+        Instant occurredAt
 ) {
 }
