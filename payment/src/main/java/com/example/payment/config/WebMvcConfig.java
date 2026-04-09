ad46a7a48affc33d1e044c38fa6fe40d071dcce1
yellow-pang <troh0322@naver.com>
2026-04-09 10:50:38 +0900

    test : 코드 리뷰 테스트

--- payment/src/main/java/com/example/payment/config/WebMvcConfig.java
+++ payment/src/main/java/com/example/payment/config/WebMvcConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 /**
- * payment 모듈에서 {@code @CurrentMember} 인자 해석기를 사용하도록 공통 Web MVC 설정을 가져온다
+ * payment 모듈에서 {@code @CurrentMember} 인자 해석기를 사용하도록 공통 Web MVC 설정을 가져온다.
  */
 @Configuration
 @Import(CurrentMemberWebConfig.class)
