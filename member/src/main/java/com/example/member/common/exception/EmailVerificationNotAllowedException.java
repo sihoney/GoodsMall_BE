db353e67afffc8acec35d7983f0d8efcb525e0db
황희원 <82200359+sihoney@users.noreply.github.com>
2026-04-15 17:43:57 +0900

    fix: 이메일 인증 관련 예외 클래스 추가

--- member/src/main/java/com/example/member/common/exception/EmailVerificationNotAllowedException.java
+++ member/src/main/java/com/example/member/common/exception/EmailVerificationNotAllowedException.java
@@ -2,6 +2,10 @@ package com.example.member.common.exception;
 
 public class EmailVerificationNotAllowedException extends RuntimeException {
 
+    public EmailVerificationNotAllowedException() {
+        super("Email verification is not allowed.");
+    }
+
     public EmailVerificationNotAllowedException(String message) {
         super(message);
     }
