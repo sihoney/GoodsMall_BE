ebcfe9d894c2e7b5c16f4f387b1dfb8b34b1d973
황희원 <82200359+sihoney@users.noreply.github.com>
2026-04-20 11:08:40 +0900

    feat: notification 최소 운영지표
    - actuator 추가
    - metrics recoder 인터페이스/구현
    - service/push 계층 계측
    - 관련 테스트 반영

---
+++ notification/src/main/java/com/example/notification/domain/enumtype/NotificationMetricReason.java
@@ -0,0 +1,9 @@
+package com.example.notification.domain.enumtype;
+
+public enum NotificationMetricReason {
+    DUPLICATE_EVENT,
+    DB_ERROR,
+    EMITTER_MISSING,
+    SEND_EXCEPTION,
+    ALREADY_PUSHED
+}
