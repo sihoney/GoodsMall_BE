004b350e22e36f593e2b58def31c953ab5801e85
yellow-pang <troh0322@naver.com>
2026-04-26 15:15:23 +0900

    refactor: payment 미사용 dircet publisher정리
    - outbox 전환 후 사용하지 않는 payment 이벤트 발행 publisher 제거

--- payment/src/main/java/com/example/payment/domain/service/SettlementCandidateCreatedEventPublisher.java
+++
@@ -1,8 +0,0 @@
-package com.example.payment.domain.service;
-
-import com.example.payment.application.event.SettlementCandidateCreatedEvent;
-
-public interface SettlementCandidateCreatedEventPublisher {
-
-    void publish(SettlementCandidateCreatedEvent event);
-}
